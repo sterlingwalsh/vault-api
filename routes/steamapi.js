f921b24227fd1e617ea034aa28a75af7afe7bcc8
Sterling Walsh <sterling.walsh.dev@gmail.com>
2019-08-09 15:47:57 -0400

    added gameDetails route

--- routes/steamapi.js
+++ routes/steamapi.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
 
-const fetch = require('node-fetch')
+const fetch = require('node-fetch');
 const FuzzySearch = require('fuzzy-search');
 const FILE_STEAM_APPLIST = './data/steam_applist.json';
 
-const URL_STEAM_APPLIST = 'http://api.steampowered.com/ISteamApps/GetAppList/v2';
-const URL_STEAM_APPDETAILS = 'https://store.steampowered.com/api/appdetails?appids=';
+const URL_STEAM_APPLIST =
+  'http://api.steampowered.com/ISteamApps/GetAppList/v2';
+const URL_STEAM_APPDETAILS =
+  'https://store.steampowered.com/api/appdetails?appids=';
 
 const util = require('util');
 const readFile = util.promisify(fs.readFile);
@@ -13,55 +15,54 @@ const readFile = util.promisify(fs.readFile);
 let searcher;
 
 const refreshIdList = async () => {
-    console.log('refresh');
-    
-    const response = await fetch(URL_STEAM_APPLIST);
-    const json = await response.json();
-    const data = json.applist.apps;
-    return await new Promise((resolve, reject) => {
-        fs.writeFile(FILE_STEAM_APPLIST, JSON.stringify(data), (err) => {
-            if(err){
-                reject('Could not save file');
-            }
-            resolve(data);
-        });
+  console.log('refresh');
+
+  const response = await fetch(URL_STEAM_APPLIST);
+  const json = await response.json();
+  const data = json.applist.apps;
+  return await new Promise((resolve, reject) => {
+    fs.writeFile(FILE_STEAM_APPLIST, JSON.stringify(data), err => {
+      if (err) {
+        reject('Could not save file');
+      }
+      resolve(data);
     });
-}
+  });
+};
 
 const getAppList = async () => {
+  try {
+    const buffer = await readFile(FILE_STEAM_APPLIST, (err, buff) => {
+      console.log('Error', err);
+      if (err) throw err;
+      resolve(buff);
+    });
+    return JSON.parse(buffer);
+  } catch (err) {
+    console.log('File read error caught', err);
+    return await refreshIdList();
+  }
+};
 
-    try{
-        const buffer = await readFile(FILE_STEAM_APPLIST, (err, buff) => {
-                        console.log('Error', err);
-                        if(err) throw err;
-                        resolve(buff)
-                    });      
-        return JSON.parse(buffer);
-    }catch(err){
-        console.log('File read error caught', err);
-        return await refreshIdList();
-    }
-}
+const searchGames = async query => {
+  if (!searcher) {
+    console.log('no searcher');
+    const appList = await getAppList();
+    console.log(appList.length);
+    searcher = new FuzzySearch(appList, ['name'], { sort: true });
+    console.log('new searcher');
+  }
 
-const searchGames = async (query) => {
-    if(!searcher){
-        console.log('no searcher');
-        const appList = await getAppList();
-        console.log(appList.length);
-        searcher = new FuzzySearch(appList, ['name'], {sort: true});
-        console.log('new searcher');
-    }
+  return searcher.search(query);
+};
 
-    return searcher.search(query);
-}
-
-const getGameInfo = async(appid) => {
-    const response = await fetch(URL_STEAM_APPDETAILS + appid)
-    return await response.json();
-}
+const getGameInfo = async appid => {
+  const response = await fetch(URL_STEAM_APPDETAILS + appid);
+  return await response.json();
+};
 
 module.exports = {
-    searchGames,
-    getGameInfo,
-    getAppList
-}
+  searchGames,
+  getGameInfo,
+  getAppList
+};
--- server.js
+++ server.js
@@ -13,23 +13,30 @@ require('dotenv').config();
 const app = express();
 app.use(bodyParser.json());
 
-const whitelist = []
+const whitelist = [];
 const corsOptions = {
-    origin: function(origin, callback){
-        if(whitelist.indexOf(origin) !== -1){
-            callback(null, true)
-        }else{
-            callback(new Error('Not allowed by CORS'));
-        }
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
     }
-}
+  }
+};
 
-if(process.env.NODE_ENV === 'development'){
-    app.use(cors());
-}else if(process.env.NODE_ENV === 'production'){
-    app.use(cors(corsOptions));
+if (process.env.NODE_ENV === 'development') {
+  app.use(cors());
+} else if (process.env.NODE_ENV === 'production') {
+  app.use(cors(corsOptions));
 }
 
 app.get('/vault', (req, res) => vault.getVault(req, res));
 
+app.post('/gameDetails', async (req, res) => {
+  const data = await steamapi.getGameInfo(req.body.id);
+  res.json(data);
+});
+
 app.listen(process.env.PORT);
+
+console.log('Server started on port', process.env.PORT);
